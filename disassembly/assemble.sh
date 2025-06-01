b5946c49ab60744b86d97c271b4c1b103079ac8b
roberto314 <100proentoffner@gmail.com>
2025-06-01 17:20:28 +0200

    assemble script updated, documentation is now as pdf avail.

--- disassembly/assemble.sh
+++ disassembly/assemble.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-NAME=eva_final2.bin
-./a09 -Beva1.bin -Leva.lst -OM03 -OFBG -DFILCHR=$FF eva_rob.asm
-dd if=eva1.bin of=tmp.bin bs=1 count=4078
-dd if=../monitor/monitor_142.bin of=mon.bin bs=1024 count=4
-dd if=../monitor/monitor_142.bin of=mtmp.bin bs=1 count=18 skip=8174 iflag=skip_bytes,count_bytes
-# use this for start to monitor
-#cat mon.bin tmp.bin mtmp.bin > $NAME
+#VERSION=eva_enh              # This is the Name of the assembly, uncomment only one!
+#VERSION=eva_ext             # This is the Name of the assembly, uncomment only one!
+VERSION=eva_orig            # This is the Name of the assembly, uncomment only one!
+
+echo Changing to Directory: $VERSION
+cd $VERSION
+../a09 -B${VERSION}.bin -L${VERSION}.lst -OM03 -OFBG -DFILCHR=$FF ${VERSION}.asm
+
+dd if=${VERSION}.bin of=tmp.bin bs=1 count=4078 # Get the Binary without Vectors
+dd if=../../monitor/monitor_142.bin of=mon.bin bs=1024 count=4 # Get only the monitor without Vectors
+dd if=../../monitor/monitor_142.bin of=mtmp.bin bs=1 count=18 skip=8174 iflag=skip_bytes,count_bytes # Get only the Vectors
+
+# use this for reset-start to monitor
+#cat mon.bin tmp.bin mtmp.bin > rom.bin # Build | Monitor | EVA Firmware | Monitor Vectors
+
 # or this for start to EVA1
-cat mon.bin eva1.bin > $NAME
+cat mon.bin ${VERSION}.bin > rom.bin # Build | Monitor | EVA Firmware | EVA Vectors
 rm tmp.bin
 rm mtmp.bin
+rm mon.bin
+cd ../
